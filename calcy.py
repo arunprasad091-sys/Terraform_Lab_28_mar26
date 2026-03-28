b0d3ce5c82c8b467362f118dab35314b05c27853
Arunprasath <arunprasad091@gmail.com>
2026-03-28 16:02:51 +0530

    Update calcy.py

--- calcy.py
+++ calcy.py
@@ -2,4 +2,3 @@ we are starting with an calcy program
 we are starting addion program
 we are ending the calcy project
 we are developing a feature in dev branch
-we are developing a feature in dev branch
